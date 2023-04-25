1c0c22f81eea04e41f058b9ac0d93b5606bcee6b
David THIBAU <david.thibau@gmail.com>
2023-04-25 20:45:38 +0200

    fix goto.sh

--- goto.sh
+++ goto.sh
@@ -8,7 +8,9 @@ rm -rf ${TARGET_DIRECTORY}/src
 rm ${TARGET_DIRECTORY}/README.md
 rm ${TARGET_DIRECTORY}/pom.xml
 rm ${TARGET_DIRECTORY}/.gitlab-ci.yml
-rm -rf ${TARGET_DIRECTORY}/ansible
+rm -rf ${TARGET_DIRECTORY}/terraform
+rm ${TARGET_DIRECTORY}/Dockerfile
+rm ${TARGET_DIRECTORY}/delivery-service.yml
 
 cp -r ./src ${TARGET_DIRECTORY}/
 cp ./README.md ${TARGET_DIRECTORY}/
@@ -16,9 +18,9 @@ cp ./pom.xml ${TARGET_DIRECTORY}/
 cp ./.gitlab-ci.yml ${TARGET_DIRECTORY}/
 cp ./.gitignore ${TARGET_DIRECTORY}/
 cp ./mvn* ${TARGET_DIRECTORY}/
-cp -r /.mvn ${TARGET_DIRECTORY}/
+cp -r ./.mvn ${TARGET_DIRECTORY}/
 cp ./settings.xml ${TARGET_DIRECTORY}/
-cp -r ./ansible ${TARGET_DIRECTORY}/
+cp -r ./terraform ${TARGET_DIRECTORY}/
 cp -r apache-jmeter-5.2.1 ${TARGET_DIRECTORY}/
 cp *.jmx ${TARGET_DIRECTORY}/
 
